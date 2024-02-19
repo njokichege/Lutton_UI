82bb7dd20e16ea24a1d996a9f78bc4890e17d2b3
Mercy Chege <22446877+njokichege@users.noreply.github.com>
2024-02-19 16:35:01 +0300

    Set the HTTPS port

--- FimiAppUI/Program.cs
+++ FimiAppUI/Program.cs
@@ -15,6 +15,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    builder.Services.AddHttpsRedirection(options =>
+    {
+        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+        options.HttpsPort = 8080; // Set your desired HTTPS port here
+    });
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
