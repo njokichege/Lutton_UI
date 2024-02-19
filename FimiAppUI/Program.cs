1995da5abb51bce3859960122b1538f723677cac
Mercy Chege <22446877+njokichege@users.noreply.github.com>
2024-02-19 16:38:45 +0300

    Remove app.UseHsts()

--- FimiAppUI/Program.cs
+++ FimiAppUI/Program.cs
@@ -21,8 +21,6 @@ if (!app.Environment.IsDevelopment())
         options.HttpsPort = 8080; // Set your desired HTTPS port here
     });
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
