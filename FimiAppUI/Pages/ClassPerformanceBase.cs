ea25146b4d965a064aac0d4c73aaef52b478724b
Mercy Njoki <mercynchege@gmail.com>
2024-03-26 11:33:30 +0300

    ClassPerformance - Genereate bulk reports

--- FimiAppUI/Pages/ClassPerformanceBase.cs
+++ FimiAppUI/Pages/ClassPerformanceBase.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.JSInterop;
+using MudBlazor;
 using Radzen.Blazor;
+using System.IO.Compression;
+using System.IO;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using PSC.Blazor.Components.Chartjs.Models.Common;
 
 namespace FimiAppUI.Pages
 {
@@ -12,6 +18,7 @@ namespace FimiAppUI.Pages
         [Inject] public IReportService ReportService { get; set; }
         [Inject] public IJSRuntime JSRuntime { get; set; }
         [Inject] public ISnackbar Snackbar { get; set; }
+        [Inject] public IClassService ClassService { get; set; }
         [Parameter] public string ClassId { get; set; }
         [Parameter] public string SessionYearId { get; set; }
         [Parameter] public string TermId { get; set; }
@@ -20,9 +27,11 @@ namespace FimiAppUI.Pages
         public List<ClassPerformanceModel> StudentsSubjectPerformanceList { get; set; } 
         public IEnumerable<StudentResultsModel> ClassStudentResults { get; set; }
         public IEnumerable<GradeModel> Grades { get; set; }
+        public ClassModel ClassDetails { get; set; }
         public MudTable<ClassPerformanceModel> mudTable;
         public bool dataIsLoaded = false;
         private int selectedRowNumber = -1;
+        public bool showDownloadProgress = false;
         protected override async Task OnInitializedAsync()
         {
             try
@@ -30,6 +39,7 @@ namespace FimiAppUI.Pages
                 await SubjectPerformanceService.InitializeStudentResults();
                 StudentsSubjectPerformance = await SubjectPerformanceService.GetStudentResultsByClass(int.Parse(ClassId), int.Parse(SessionYearId), int.Parse(TermId), int.Parse(ExamTypeId));
                 Grades = await GradeService.GetAllGrades();
+                ClassDetails = await ClassService.GetClassById(int.Parse(ClassId));
             }
             catch(HttpRequestException ex)
             {
@@ -62,9 +72,50 @@ namespace FimiAppUI.Pages
         }
         public async Task GenerateAllReportForms()
         {
-            foreach(var student in StudentsSubjectPerformanceList)
-            {
+            showDownloadProgress = true;
 
+            Dictionary<byte[],int> studentReportBytes = new Dictionary<byte[],int>();
+            foreach (var student in StudentsSubjectPerformanceList)
+            {
+                byte[] reportData = await ReportService.StudentReportCardBytes(student.StudentNumber, SessionYearId, TermId, ExamTypeId);
+                studentReportBytes.Add(reportData, student.StudentNumber);
+            }
+
+            byte[] zippedFiles;
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                {
+                    foreach (var fileBytes in studentReportBytes)
+                    {
+                        var entry = archive.CreateEntry($"ReportForm_{ClassDetails.Form.Form}{ClassDetails.Stream.Stream}_{fileBytes.Value}" + ".pdf");
+                        using (var entryStream = entry.Open())
+                        {
+                            await entryStream.WriteAsync(fileBytes.Key);
+                        }
+                    }
+                }
+                zippedFiles = memoryStream.ToArray();
+            }
+
+            if (zippedFiles == null)
+            {
+                Snackbar.Add("Failed to load report form", MudBlazor.Severity.Error);
+            }
+            else
+            {
+                FileUtil.SaveAs(JSRuntime, $"{ClassDetails.Form.Form}{ClassDetails.Stream.Stream}.zip", zippedFiles);
+            }
+            showDownloadProgress = false;
+        }
+        public static class FileUtil
+        {
+            public async static Task SaveAs(IJSRuntime js, string filename, byte[] data)
+            {
+                await js.InvokeAsync<object>(
+                    "saveAsFile",
+                    filename,
+                    Convert.ToBase64String(data));
             }
         }
         public async Task StudentRowClickEventAsync(TableRowClickEventArgs<ClassPerformanceModel> tableRowClickEventArgs)
@@ -73,14 +124,14 @@ namespace FimiAppUI.Pages
 
             byte[] reportData = await ReportService.StudentReportCardBytes(tableRowClickEventArgs.Item.StudentNumber,SessionYearId,TermId,ExamTypeId);
             string mimeType = "application/pdf";
-            string fileName = $"ReportForm_{tableRowClickEventArgs.Item.StudentNumber}";
+            string fileName = $"ReportForm_{ClassDetails.Form.Form}{ClassDetails.Stream.Stream}_{tableRowClickEventArgs.Item.StudentNumber}";
             if (reportData == null)
             {
                 Snackbar.Add("Failed to load report form", MudBlazor.Severity.Error);
             }
             else
             {
-                JSRuntime.InvokeVoidAsync("saveFile", Convert.ToBase64String(reportData), mimeType, fileName);
+                await JSRuntime.InvokeVoidAsync("saveFile", Convert.ToBase64String(reportData), mimeType, fileName);
             }
         }
         public string SelectedRowClassFunc(ClassPerformanceModel element, int rowNumber)
--- FimiAppUI/wwwroot/lib/BlazorDownloadFile.js
+++ FimiAppUI/wwwroot/lib/BlazorDownloadFile.js
@@ -11,3 +11,11 @@
             document.body.removeChild(link);
         });
 }
+function saveAsFile(filename, bytesBase64) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + bytesBase64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
